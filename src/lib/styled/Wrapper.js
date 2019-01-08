aafb0ece9638f227b9e9948492ca6609ca58e704
Kevin Lee <kevin.lee@globalrelay.net>
2019-01-08 10:57:43 -0800

    add border color to tag input

--- src/lib/styled/Wrapper.js
+++ src/lib/styled/Wrapper.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   background: white;
+  border: 1px solid lightblue;
   box-shadow: 3px 3px 10px rgba(0,0,0, 0.1);
   font-family: 'Hind', sans-serif;
   font-weight: 400;
